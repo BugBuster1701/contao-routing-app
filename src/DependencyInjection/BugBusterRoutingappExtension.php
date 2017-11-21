b99b51e6eb5b6825332ee2c2ef339ad44ee405ac
Glen Langer <contao.modul@glen-langer.de>
2017-11-21 01:06:57 +0100

    Kommentare entfernt

--- src/DependencyInjection/BugBusterRoutingappExtension.php
+++ src/DependencyInjection/BugBusterRoutingappExtension.php
@@ -1,12 +1,6 @@
 <?php
 
-/*
- * This file is part of Contao.
- *
- * Copyright (c) 2005-2017 Leo Feyer
- *
- * @license LGPL-3.0+
- */
+
 
 namespace BugBuster\RoutingappBundle\DependencyInjection;
 
@@ -17,8 +11,6 @@ use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
 /**
  * Adds the bundle services to the container.
- *
- * @author Andreas Schempp <https://github.com/aschempp>
  */
 class BugBusterRoutingappExtension extends Extension
 {
